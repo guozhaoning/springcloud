ce5d7bfb69606b9aef3874c9dd9b3a887983e908
guozhaoning <guozhaoning@foxmail.com>
2021-02-10 23:27:27 +0800

    新增nacosConfig配置

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/njhr/springcloud/NacosConfigMain3377.java
@@ -0,0 +1,16 @@
+package com.njhr.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author guozhaoning
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosConfigMain3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigMain3377.class, args);
+    }
+}
